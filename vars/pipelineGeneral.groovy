e46c13ad30bf1fff1ef5c50eea7275c167dd112a
davidruiz <davidruiz@periferia-it.com>
2025-01-14 15:43:49 -0500

    Updated pipelineGeneral

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -1,21 +1,38 @@
 @Library('lb_buildartefacto')
 @Library('lb_analisissonarqube')
-def pipelineGeneral(){
+def pipelineGeneral() {
     pipeline {
-    agent any
-    stages{
-        stage('Construir app'){
-            steps{
-                lb_buildartefacto.clone()
-                lb_buildartefacto.install()
+        agent any
+        stages {
+            stage('Preparar') {
+                steps {
+                    script {
+                        echo "Preparando el entorno..."
+                        //body()
+                    }
+                }
             }
-        }
-        stage('Analisis Sonarqube'){
-            steps{
-                lb_analisissonarqube.testCoverage()
-                lb_analisissonarqube.analisisSonar() 
+            stage('Construir') {
+                steps {
+                    script {
+                        echo "Construyendo el proyecto..."
+                    }
+                }
+            }
+            stage('Pruebas') {
+                steps {
+                    script {
+                        echo "Ejecutando pruebas..."
+                    }
+                }
+            }
+            stage('Desplegar') {
+                steps {
+                    script {
+                        echo "Desplegando la aplicación..."
+                    }
+                }
             }
         }
     }
-}
 }
