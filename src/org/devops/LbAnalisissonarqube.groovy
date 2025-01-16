c62ae1a344a3f7862c44a37afa1d4dbf9c1260f7
davidruiz <davidruiz@periferia-it.com>
2025-01-16 08:41:06 -0500

    working on sonar stage

--- src/org/devops/LbAnalisissonarqube.groovy
+++ src/org/devops/LbAnalisissonarqube.groovy
@@ -3,15 +3,12 @@ package org.devops
 class LbAnalisissonarqube {
     static void analizarCodigo(steps, Map parametros = [:]) {
         steps.sh 'echo Analizando código con SonarQube...'
-        //withSonarQubeEnv('ServerSonarqube') {
-                        sonar =  "${scannerHome}/bin/sonar-scanner \
-                            -Dsonar.projectKey=analisisTermometro \
-                            -Dsonar.projectName=analisisTermometro \
-                            -Dsonar.sources=src/main/java \
-                            -Dsonar.java.binaries=target/classes \
-                            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml".execute().text
-        //            }
+        sonar =  "${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=analisisTermometro \
+            -Dsonar.projectName=analisisTermometro \
+            -Dsonar.sources=src/main/java \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml".execute().text
 
-        // Lógica de análisis de SonarQube aquí
     }
 }
