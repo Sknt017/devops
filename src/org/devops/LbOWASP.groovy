ac1dcc73a7324614c36d7da70bcb466e0557b140
davidruiz <davidruiz@periferia-it.com>
2025-01-22 09:05:03 -0500

    config pipeline

--- src/org/devops/LbOWASP.groovy
+++ src/org/devops/LbOWASP.groovy
@@ -2,10 +2,11 @@ package org.devops
 
 class LbOWASP {
     static String analizarProyecto(steps) {
+        //Descarga de OWASP ZAProxy
         def networkName = 'jenkinsOwasp'
         def imageName = 'zaproxy/zap-stable'
         def zapContainerName = 'owaspimagen-container'
-        def imageExists = steps.sh(script: "docker images -q $imageName",returnStdout: true).trim()
+        def imageExists = steps.sh(script: "docker images -q ${imageName}",returnStdout: true ).trim()
         if(imageExists.isEmpty()){
             steps.sh "docker pull $imageName"
         } else {
