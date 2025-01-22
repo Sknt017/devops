967741bcc7d4f9303959dfd2f29923ec563e1895
davidruiz <davidruiz@periferia-it.com>
2025-01-22 09:24:46 -0500

    config pipeline

--- src/org/devops/LbOWASP.groovy
+++ src/org/devops/LbOWASP.groovy
@@ -18,10 +18,12 @@ class LbOWASP {
         def dockerHubUsername = 'davidruiz212'
         def crudspringbootImageName = 'termometro'
         def crudspringbootContainerName = 'termometro-container'
+        //custom port config
+        def crudspringbootContainerNamePort = "8081"
         steps.sh "docker stop ${crudspringbootContainerName} || true"
         steps.sh "docker rm ${crudspringbootContainerName} || true"
-        steps.sh "docker run -d --name ${crudspringbootContainerName} --network=${networkName} -p 3500:8080 --user root ${dockerHubUsername}/${crudspringbootImageName}"
-        def targetURL = "http://${steps.env.iphost}:3500"
+        steps.sh "docker run -d --name ${crudspringbootContainerName} --network=${networkName} -p ${crudspringbootContainerNamePort}:${crudspringbootContainerNamePort} --user root ${dockerHubUsername}/${crudspringbootImageName}"
+        def targetURL = "http://${steps.env.iphost}:${crudspringbootContainerNamePort}"
         steps.sh "docker run --rm -v fullscancrudspringboot:/zap/wrk/:rw --user root --network=${networkName} -t $imageName zap-full-scan.py -t ${targetURL} -r fullScanReport.html -I"
         steps.sh "docker stop ${zapContainerName}"
         steps.sh "docker rm ${zapContainerName}"
